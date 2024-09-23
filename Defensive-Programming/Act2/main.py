5287c143761847fda664265a23e223ff51efa8cf
Nier'seDev <nienachuraru@yahoo.com>
2024-09-23 19:54:43 +0800

    token

--- Defensive-Programming/Act2/main.py
+++ Defensive-Programming/Act2/main.py
@@ -1,9 +1,14 @@
+class Store:
+    def __init__(self, name):
+        self.name = name
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
 class ShoppingCart:
     def __init__(self, ):
-
-
-def main():
 
 
 if __name__ = "__main__":
