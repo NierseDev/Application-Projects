3f2ff008b074894ae4d1999d6ebb32e9786a6e6b
Nier'seDev <nienachuraru@yahoo.com>
2024-09-24 23:57:46 +0800

    Task - Done

--- Defensive-Programming/Act2/main.py
+++ Defensive-Programming/Act2/main.py
@@ -3,26 +3,6 @@ class Product:
         self.name = name
         self.price = price
 
-class ShopInventory:
-    def __init__(self):
-        self.products = {}
-
-    def add_product(self, product, quantity):
-        if product.name in self.products:
-            self.products[product.name]['quantity'] += quantity
-        else:
-            self.products[product.name] = {'product': product, 'quantity': quantity}
-    
-    def subtract_from_inventory(self, product, quantity):
-        if product.name in self.products:
-            if self.products[product.name]['quantity'] >= quantity:
-                self.products[product.name]['quantity'] -= quantity
-            else:
-                print("Not enough quantity in stock!")
-        else:
-            print("Product is not available in stock!")
-
-
 class Balance:
     def __init__(self, balance):
         self.balance = balance
@@ -34,26 +14,31 @@ class Balance:
         self.balance -= amount
 
     def get_balance(self):
+        print(f"Your current balance is: {self.balance}")
         return self.balance
 
 
 class ShoppingCart:
-    def __init__(self, user_balance, shop_inventory):
+    def __init__(self, user_balance):
         self.user_balance = user_balance
-        self.shop_inventory = shop_inventory
         self.products = []
     
-    def add_product(self, product):
-        self.products.append(product)
+    def add_product(self, name, price):
+        self.products.append(Product(name, price))
 
-    def remove_product(self, product):
-        self.products.remove(product)
+    def remove_product(self, product, num=1):
+        try:
+            for _ in range(num):
+                self.products.remove(product)
+        except ValueError:
+            print("\nProduct not found in cart. Please try again!")
     
     def list_products(self):
         if len(self.products) == 0:
-            print("No products available!")
+            print(f"\nIn Cart:\nEmpty")
         else:
-            for index, product in enumerate(self.products):
+            print("\nIn Cart:")
+            for index, product in enumerate(self.products, start=1):
                 print(index, product.name, product.price)
     
     def purchase_products(self):
@@ -65,55 +50,62 @@ class ShoppingCart:
         else:
             self.user_balance.subtract_from_balance(total_price)
             for product in self.products:
-                self.shop_inventory.subtract_from_inventory(product, 1)
+                self.remove_product(product, self.products.count(product))
             self.products = []
             print("You have purchased the products in the cart")
 
 def main():
-    user_balance = Balance(0)
+    user_balance = Balance(100)
     shopping_cart = ShoppingCart(user_balance)
 
-    # Product 1: Keyboard
-    product1 = Product("Keyboard", 1399)
-    shopping_cart.add_product(product1)
+    ### Debugging Code ###
+    # # Product 1: Keyboard
+    # product1 = Product("Keyboard", 1399)
+    # shopping_cart.add_product(product1)
+    # # Product 2: Mouse
+    # product2 = Product("Mouse", 399)
+    # shopping_cart.add_product(product2)
 
-    # Product 2: Mouse
-    product2 = Product("Mouse", 399)
-    shopping_cart.add_product(product2)
-
-    # Choose User
-
-    # Admin (ShopInventory)
-
-    # Customer
+    # Customer Input
     print("Hello, Welcome to my store!")
+    shopping_cart.list_products()
 
     while True:
-        shopping_cart.list_products()
-        print("What would you like to do?")
-        print("1. Add product to cart")
-        print("2. Remove product from cart")
-        print("3. List products in cart")
-        print("4. Purchase products")
-        print("5. Deposit to Balance")
+        print("""
+What would you like to do?
+              
+    1. Add product to cart
+    2. Remove product from cart
+    3. List products in cart
+    4. Purchase products
+    5. Deposit to Balance
+    6. Check Balance
+        """)
 
-        user_input = int(input("Enter Option No.: "))
+        user_input = int(input("Enter Option: "))
         match user_input:
             case 1:
-                input_product = input("Enter Name of Product: ")
-                input_price = input("Enter the Product Price: ")
+                input_product = input("\nEnter Name of Product: ")
+                input_price = int(input("Enter the Product Price: "))
 
-                shopping_cart.add_product()
+                shopping_cart.add_product(input_product, input_price)
             case 2:
-                input_product = input("Enter Name of Product: ")
-                shopping_cart.remove_product(Product(input_product, 0))
+                input_product = input("\nEnter Name of Product: ")
+                for product in shopping_cart.products:
+                    if product.name == input_product:
+                        shopping_cart.remove_product(product)
+                        break
+                    else:
+                        print("\nProduct not found in cart. Please try again!")
             case 3:
                 shopping_cart.list_products()
             case 4:
                 shopping_cart.purchase_products()
             case 5:
-                amount = int(input("Enter Amount to Deposit: "))
-                shopping_cart.user_balance.add_to_balance(amount)    
+                amount = int(input("\nEnter Amount to Deposit: "))
+                shopping_cart.user_balance.add_to_balance(amount)
+            case 6:
+                shopping_cart.user_balance.get_balance()
             case _:
                 print("Invalid Option. Please try again!")
 
